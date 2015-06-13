f18ccc03a8e53443e16e5c70e5db50e650f3577d
snaika <sean.coriolan@epitech.eu>
2015-06-13 14:38:05 +0200

    addproduct test

--- addProduct.php
+++ addProduct.php
@@ -1,1 +1,15 @@
-<h1>Blank Page</h1>
+<?php include("includes/header.php"); ?>
+
+<?php include("includes/menu.php"); ?>
+
+<div class="l-container">
+
+	<h2 class="text-left">Ajoutez un nouveau produit</h2>
+	<div class="section" style="text-align:center;">
+		
+	</div>
+
+</div>
+
+
+<?php include("includes/footer.php"); ?>
