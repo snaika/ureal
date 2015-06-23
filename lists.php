8ef553186e736ef1ce118e57e00d68cb30b62311
Kyosukke <kyosukke1@gmail.com>
2015-06-23 15:53:10 +0200

    ListInfo Added

--- lists.php
+++ lists.php
@@ -23,21 +23,45 @@
 	          <td>A vendre</td>
 	          <td>21-06-2015</td>
 	          <td>21-06-2015</td>
-	          <td>Publique</td>
+	          <td>
+				<select>
+					<option>Publique</option>
+					<option>Privée</option>
+				</select>
+			  </td>
+			  <td><img src="images/add.png" alt=""></td>
+			  <td><img src="images/delete.png" alt=""></td>
+			  <td><a href="./listInfo.php"><img src="images/update.png" alt=""></a></td>
 	        </tr>
 	        <tr data-href="./listDisplay.php">
 	          <th scope="row">2</th>
 	          <td>Mes envies</td>
 	          <td>10-06-2015</td>
 	          <td>18-06-2015</td>
-	          <td>Privée</td>
+	          <td>
+  				<select>
+					<option>Publique</option>
+					<option>Privée</option>
+				</select>
+			  </td>
+			  <td><img src="images/add.png" alt=""></td>
+			  <td><img src="images/delete.png" alt=""></td>
+			  <td><a href="./listInfo.php"><img src="images/update.png" alt=""></a></td>
 	        </tr>
 	        <tr data-href="./listDisplay.php">
 	          <th scope="row">3</th>
 	          <td>Course de la semaine</td>
 	          <td>13-06-2015</td>
 	          <td>15-06-2015</td>
-	          <td>Privée</td>
+	          <td>
+  				<select>
+					<option>Publique</option>
+					<option>Privée</option>
+				</select>
+			  </td>
+			  <td><img src="images/add.png" alt=""></td>
+			  <td><img src="images/delete.png" alt=""></td>
+			  <td><a href="./listInfo.php"><img src="images/update.png" alt=""></a></td>
 	        </tr>
 	      </tbody>
 	    </table>
